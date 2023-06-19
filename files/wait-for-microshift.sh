c0caaa6c9f04786ddfbc6caefccf1aa5b4834655
Daniel Pawlik <dpawlik@redhat.com>
2023-06-19 17:24:13 +0200

    Add post step to configure coredns after Microshift restart
    After reboot, when the Microshift service is restarted, the coredns
    configuration has been changed to default one, which does not have
    configuration to forward the DNS queries to dnsmasq.
    Also changed location of the wait-for-microshift.sh script to
    /usr/local/bin.

--- files/wait-for-microshift.sh
+++ files/wait-for-microshift.sh
@@ -4,7 +4,15 @@ for i in {1..20}; do
     echo "Ensuring that containers are spawned... ${i}"
     count=$(oc get pods --all-namespaces | grep openshift | grep -vic 'running')
     if [ "$count" -eq "0" ]; then
-        echo "Microshift is deployed, we can continue..."
+        # Ensure that all pods are in expected count
+        for ns in $(oc get pods --all-namespaces | grep openshift | awk '{print $1}'); do
+            echo "Checking pods from namespace: $ns";
+            if ! kubectl -n "$ns" wait --for=condition=Ready pod --all; then
+                echo -e "\nThe namespace $ns pods count does not match. Waiting..."
+                sleep 15
+            fi
+        done
+        echo -e "\nMicroshift is deployed, we can continue..."
         break
     else
         echo "The Microshift containers are not ready..."
