1d30e3a281322fc965e833fbb89e08fd6e3402f1
Daniel Pawlik <dpawlik@redhat.com>
2023-06-21 10:47:12 +0200

    Improve wait-for-microshift.sh script
    The new change is using Kubernetes mechanism "kubectl wait" that is
    waiting for the pods to pass the condition.

--- files/wait-for-microshift.sh
+++ files/wait-for-microshift.sh
@@ -4,10 +4,23 @@ for i in {1..20}; do
     echo "Ensuring that containers are spawned... ${i}"
     count=$(oc get pods --all-namespaces | grep openshift | grep -vic 'running')
     if [ "$count" -eq "0" ]; then
-        echo "Microshift is deployed, we can continue..."
-        break
+        # Ensure that all pods are in expected count
+        for ns in $(oc get pods --all-namespaces | grep openshift | awk '{print $1}'); do
+            echo "Checking pods from namespace: $ns";
+            if ! kubectl -n "$ns" wait --for=condition=Ready pod --all; then
+                echo -e "\nThe namespace $ns pods count does not match. Waiting..."
+                sleep 15
+            fi
+        done
+        echo -e "\nMicroshift is deployed, we can continue..."
+        exit 0
     else
         echo "The Microshift containers are not ready..."
         sleep 15
     fi
 done
+
+# Normally, the script should finished in the for loop, but if it's not
+# it should exit with an error.
+echo -e "\nSomthing is not deployed in Microshift. Exit!\n"
+exit 1
